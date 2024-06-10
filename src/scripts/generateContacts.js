2e9df26304260ac9066e6c4937a266a5b321d848
Yevhen <igrovoy88888@gmail.com>
2024-06-10 14:17:53 +0300

    Up

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,28 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    const newContacts = [];
+    for (let i = 0; i < number; i++) {
+      newContacts.push(createFakeContact());
+    }
+
+    const updatedContacts = [...contacts, ...newContacts];
+
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(updatedContacts, null, 2),
+      'utf-8',
+    );
+    console.log(`Successfully added ${number} new contacts.`);
+  } catch (error) {
+    console.error('Error updating contacts:', error);
+  }
+};
 
 await generateContacts(5);
